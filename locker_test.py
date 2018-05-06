bd94a06f64ef8858055de0bf5817d849fdfcc153
Monroe100 <michelle1hunnid@gmail.com>
2018-05-06 09:01:59 +0300

    updated locker test

--- locker_test.py
+++ locker_test.py
@@ -1,3 +1,6 @@
 import unittest #importing the unittest module
 from locker import userdata #importing the userdata class
 class Testuserdata(unittest.TestCase):
+    '''
+    This test class defines test cases for the behaviour of the userdata class
+    '''
