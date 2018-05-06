660359553768a78499d7005191baf9252a2115b6
Monroe100 <michelle1hunnid@gmail.com>
2018-05-06 09:10:19 +0300

    updated locker test

--- locker_test.py
+++ locker_test.py
@@ -4,3 +4,15 @@ class Testuserdata(unittest.TestCase):
     '''
     This test class defines test cases for the behaviour of the userdata class
     '''
+    def setUp(self):
+        '''
+        set up method to run before each test cases
+        '''
+        self.new_data = userdata("monroe","babygirl","facebook")
+        #create data object
+
+        def test_init(self):
+            '''
+            used to test if the object has been initialized properly
+            '''
+            
