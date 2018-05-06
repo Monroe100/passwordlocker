ca591bee7347e11f161775a500465ce9994fc335
Monroe100 <michelle1hunnid@gmail.com>
2018-05-06 08:53:07 +0300

    updated locker test

--- locker.py
+++ locker.py
@@ -1,10 +1,13 @@
-import os,csv,pyperclip
-
-class user data:
+# import os,csv,pyperclip
+class userdata:
     '''
-    Class that contains the users data such as username
+    Class that contains the users data such as username, password and the category of the account(gmail,facebook etc)
     '''
+    data_list = [] #empty data list
+    
     def __init__(self,username,password,category):
         self.username = username
         self.password = password
         self.category = category
+
+
