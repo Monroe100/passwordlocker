6c719cb94af16b477043294bddf07e8e3974834c
Monroe100 <michelle1hunnid@gmail.com>
2018-05-06 10:48:15 +0300

    added a function in locker.py

--- locker.py
+++ locker.py
@@ -4,10 +4,18 @@ class userdata:
     Class that contains the users data such as username, password and the category of the account(gmail,facebook etc)
     '''
     data_list = [] #empty data list
-    
+
     def __init__(self,username,password,category):
         self.username = username
         self.password = password
         self.category = category
+
+
+    def save_data(self):
+        '''
+        save_data method saves data object into the new_data
+        '''
+
+        userdata.new_data.append(self)
 
 
